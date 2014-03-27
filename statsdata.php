7eef4722299b66e6d202eb06474ef8000d5ba477
Quetzacoalt91 <thomas.nabord@prestashop.com>
2014-03-27 17:19:09 +0100

    / MO statsdata : min ps_versions_compliancy modified (1.5.6.1 => 1.6)

--- statsdata.php
+++ statsdata.php
@@ -33,7 +33,7 @@ class StatsData extends Module
 	{
 		$this->name = 'statsdata';
 		$this->tab = 'analytics_stats';
-		$this->version = 1.2;
+		$this->version = '1.3';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
@@ -42,7 +42,7 @@ class StatsData extends Module
 
 		$this->displayName = $this->l('Data mining for statistics');
 		$this->description = $this->l('This module must be enabled if you want to use statistics.');
-		$this->ps_versions_compliancy = array('min' => '1.5.6.1', 'max' => _PS_VERSION_);
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
 	}
 
 	public function install()
